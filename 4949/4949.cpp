0f0f4139a5f4037dcfee35ea1d25f065dd07cf78
SYJoe <sungyeon52@gmail.com>
2021-08-21 07:48:14 +0000

    Add 4949 #2

--- 4949/4949.cpp
+++ 4949/4949.cpp
@@ -7,27 +7,35 @@ int marks(char* str, int end)
 	int small_close = 0;
 	int big_open = 0;
 	int big_close = 0;
-	
+	printf("%s\n", str);
 	for(int i = 0 ; i < end; i++)
 	{
 		if(str[i] == '(')
 		{
 			small_open++;
 		}
-		else if(str[i] == ')'&&small_open)
+		else if(str[i] == ')' && small_open > small_close)
 		{
+			if(big_open != big_close)
+			{
+				return 0;
+			}
 			small_close++;
 		}
 		else if(str[i] == '[')
 		{
 			big_open++;
 		}
-		else if(str[i] == ']'&&big_open)
+		else if(str[i] == ']' && big_open > big_close)
 		{
+			if(small_open != small_close)
+			{
+				return 0;
+			}
 			big_close++;
 		}
+		printf("%d %d %d %d\n", small_open, small_close, big_open, big_close);
 	}
-	
 	if(small_open == small_close && big_open == big_close)
 	{
 		return 1;	
@@ -38,37 +46,16 @@ int marks(char* str, int end)
 	}
 }
 
-void print_result(char* str)
-{
-	int i = 0;
-	int end;
-	int result;
-	while(str[i] != '\0')
-	{
-		if(str[i] == '.')
-		{
-			end = i+1;
-			result = marks(str, end);
-			if(result)
-			{
-				printf("yes\n");
-			}
-			else
-			{
-				printf("no\n");
-			}
-		}
-		i++;
-	}
-}
-
 int main()
 {
 	char* str;
+	int i = 0;
+	int end;
+	int result;
 	
 	while(1)
 	{
-		str = (char*)malloc(101*sizeof(char));
+		char str[101];
 		fgets(str, sizeof(str), stdin);
 		if(str[0] == '.')
 		{
@@ -76,8 +63,26 @@ int main()
 		}
 		else
 		{
-			print_result(str);
+			i = 0;
+			while(str[i-1] != '.' )
+			{
+				if(str[i] == '.')
+				{
+					end = i;
+					result = marks(str, end);
+					if(result)
+					{
+						printf("yes\n");
+					}
+					else
+					{
+						printf("no\n");
+					}
+					getchar();
+					break;
+				}
+				i++;
+			}
 		}		
-		free(str);
 	}
 }
